46336d1b2053c790860e4e0a40fd265b7f6e90fc
zhevas <gzhevas@gmail.com>
2022-05-29 20:16:50 +0400

    Time: 698 ms (11.97%), Space: 144.7 MB (5.15%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -12,10 +12,18 @@ class Solution {
         }
         return max;
     }
-    
+
     private boolean hasSameLetter(String str1, String str2) {
-       return str1.chars().anyMatch(it -> str2.indexOf(it) > -1);
+        int bitmask1 = 0, bitmask2 = 0;
+        for (char ch : str1.toCharArray())
+            bitmask1 |= 1 << bitNumber(ch);
+        for (char ch : str2.toCharArray())
+            bitmask2 |= 1 << bitNumber(ch);
+
+        return (bitmask1 & bitmask2) != 0;
     }
-    
-    
+
+    private int bitNumber(char ch) {
+        return (int)ch - (int)'a';
+    }
 }
